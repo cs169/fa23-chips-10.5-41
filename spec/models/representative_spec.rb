45eceb2281c0a8321e28d3336d3220146154b4fe
Jonathan Garcia <jcgarcia02@berkeley.edu>
2023-11-19 01:49:34 +0000

    added test for duplicate representatives

--- spec/models/representative_spec.rb
+++ spec/models/representative_spec.rb
@@ -5,6 +5,7 @@ describe Representative do
   describe 'civic api to representative params' do
     before :each do
       @rep_info = double('rep_info')
+      @existing_rep = Representative.create!(name: 'John Doe', ocdid: 'ocd-division/country:us/state:ca/place:example_city', title: 'Mayor')
     end
     it 'creates representatives from rep_info' do
       allow(@rep_info).to receive(:officials).and_return([
@@ -22,6 +23,20 @@ describe Representative do
 
       Representative.civic_api_to_representative_params(@rep_info)
     end
+  end
+  it 'doesnt duplicate representatives' do
+    allow(@rep_info).to receive(:officials).and_return([
+      double('official1', name: 'John Doe')
+    ])
+
+    allow(@rep_info).to receive(:offices).and_return([
+      double('office1', name: 'Mayor', "official_indices": [0], division_id: 'ocd-division/country:us/state:ca/place:example_city')
+    ])
+
+    expect(@existing_rep).not_to receive(:update!)
+    expect(Representative).not_to receive(:create!)
+
+    Representative.civic_api_to_representative_params(@rep_info)
   end
 end
 
