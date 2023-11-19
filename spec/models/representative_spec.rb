93c5cd36be77038c435c5a0dca49c4d6ddd26296
Jonathan Garcia <jcgarcia02@berkeley.edu>
2023-11-19 02:21:38 +0000

    ran rubocop -A, still more to fix

--- spec/models/representative_spec.rb
+++ spec/models/representative_spec.rb
@@ -1,42 +1,51 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 require 'spec_helper'
 
 describe Representative do
   describe 'civic api to representative params' do
-    before :each do
+    before do
       @rep_info = double('rep_info')
-      @existing_rep = Representative.create!(name: 'Chris Traeger', ocdid: 'ocd-division/country:us/state:ca/place:example_city', title: 'Clerk')
+      @existing_rep = described_class.create!(name: 'Chris Traeger',
+                                              ocdid: 'ocd-division/country:us/state:ca/place:example_city', title: 'Clerk')
     end
+
     it 'creates representatives from rep_info' do
       allow(@rep_info).to receive(:officials).and_return([
-        double('official1', name: 'John Doe'),
-        double('official2', name: 'Jane Smith')
-      ])
+                                                           double('official1', name: 'John Doe'),
+                                                           double('official2', name: 'Jane Smith')
+                                                         ])
 
       allow(@rep_info).to receive(:offices).and_return([
-        double('office1', name: 'Mayor', "official_indices": [0], division_id: 'ocd-division/country:us/state:ca/place:example_city'),
-        double('office2', name: 'Governor', "official_indices": [1], division_id: 'ocd-division/country:us/state:ca')
-      ])
+                                                         double('office1', name: 'Mayor', official_indices: [0],
+division_id: 'ocd-division/country:us/state:ca/place:example_city'),
+                                                         double('office2', name: 'Governor', official_indices: [1],
+division_id: 'ocd-division/country:us/state:ca')
+                                                       ])
 
-      expect(Representative).to receive(:create!).with({ name: 'John Doe', ocdid: 'ocd-division/country:us/state:ca/place:example_city', title: 'Mayor' })
-      expect(Representative).to receive(:create!).with({ name: 'Jane Smith', ocdid: 'ocd-division/country:us/state:ca', title: 'Governor' })
+      expect(described_class).to receive(:create!).with({ name: 'John Doe',
+ocdid: 'ocd-division/country:us/state:ca/place:example_city', title: 'Mayor' })
+      expect(described_class).to receive(:create!).with({ name: 'Jane Smith', ocdid: 'ocd-division/country:us/state:ca',
+title: 'Governor' })
 
-      Representative.civic_api_to_representative_params(@rep_info)
+      described_class.civic_api_to_representative_params(@rep_info)
     end
+
     it 'doesnt duplicate representatives' do
       allow(@rep_info).to receive(:officials).and_return([
-        double('official1', name: 'Chris Traeger')
-      ])
-  
+                                                           double('official1', name: 'Chris Traeger')
+                                                         ])
+
       allow(@rep_info).to receive(:offices).and_return([
-        double('office1', name: 'Clerk', "official_indices": [0], division_id: 'ocd-division/country:us/state:ca/place:example_city')
-      ])
-  
+                                                         double('office1', name: 'Clerk', official_indices: [0],
+division_id: 'ocd-division/country:us/state:ca/place:example_city')
+                                                       ])
+
       expect(@existing_rep).not_to receive(:update!)
-      expect(Representative).not_to receive(:create!)
-  
-      Representative.civic_api_to_representative_params(@rep_info)
+      expect(described_class).not_to receive(:create!)
+
+      described_class.civic_api_to_representative_params(@rep_info)
     end
   end
 end
-
