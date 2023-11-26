25c38c272aee638cb06a1b43303042cc0d9e2835
Jonathan Garcia <jcgarcia02@berkeley.edu>
2023-11-26 21:33:40 +0000

    fixed rubocop errors

--- features/step_definitions/oauth_steps.rb
+++ features/step_definitions/oauth_steps.rb
@@ -1,29 +1,31 @@
-When("I click on the Google login button") do
+# frozen_string_literal: true
+
+When('I click on the Google login button') do
   # Implement the logic to click on the Google login button
 end
 
-Then("I should be redirected to the Google OAuth page") do
+Then('I should be redirected to the Google OAuth page') do
   # Assuming the URL of the Google OAuth page starts with "https://accounts.google.com/o/oauth2/auth"
-  expected_url_prefix = "https://accounts.google.com/o/oauth2/auth"
+  expected_url_prefix = 'https://accounts.google.com/o/oauth2/auth'
 
   # Wait for the redirection to happen and check the current URL
-  expect(page).to have_current_path(%r{^#{Regexp.escape(expected_url_prefix)}}, wait: 10)
+  expect(page).to have_current_path(/^#{Regexp.escape(expected_url_prefix)}/, wait: 10)
 end
 
-When("I log in with valid Google credentials") do
+When('I log in with valid Google credentials') do
   # Mock the OmniAuth response for successful authentication
   OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
-    provider: 'google_oauth2',
-    uid: '12345',
-    info: { email: 'user@example.com' }
-  })
+                                                                       provider: 'google_oauth2',
+                                                                       uid:      '12345',
+                                                                       info:     { email: 'user@example.com' }
+                                                                     })
   click_on 'Login with Google'
 end
 
-Then("I should be redirected back to the application") do
+Then('I should be redirected back to the application') do
   # Implement the assertion for successful redirection
 end
 
-Then("I should be logged in") do
+Then('I should be logged in') do
   # Implement the assertion for user login status
 end
