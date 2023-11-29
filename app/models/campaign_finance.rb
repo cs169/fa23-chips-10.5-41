1f27695e18cfb26184e35b13348520c8e2eaffa4
Jonathan Garcia <jcgarcia02@berkeley.edu>
2023-11-29 03:08:54 +0000

    started test for campaign finance model

--- app/models/campaign_finance.rb
+++ app/models/campaign_finance.rb
@@ -3,7 +3,7 @@
 class CampaignFinance < ApplicationRecord
   attr_accessor :cycle, :category
 
-  def self.propublica_api_to_representatives(cycle, category)
+  def self.propublica_api_to_representatives(rep_info)
     reps = []
   end
 end
