e4c091d79d02b78954c9c7e47f6937455595f361
Victor Shen <vshen@berkeley.edu>
2023-12-06 03:10:10 +0000

    style final

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -3,10 +3,12 @@
 class NewsItem < ApplicationRecord
   belongs_to :representative
   has_many :ratings, dependent: :delete_all
-  validates :issue, presence: true, inclusion: { in: ['Free Speech', 'Immigration', 'Terrorism', 'Social Security and Medicare',
-                                                      'Abortion', 'Student Loans', 'Gun Control', 'Unemployment',
-                                                      'Climate Change', 'Homelessness', 'Racism', 'Tax Reform', 'Net Neutrality',
-                                                      'Religious Freedom', 'Border Security', 'Minimum Wage', 'Equal Pay'] }
+  validates :issue, presence: true, inclusion: { in: ['Free Speech', 'Immigration', 'Terrorism',
+                                                      'Social Security and Medicare', 'Abortion',
+                                                      'Student Loans', 'Gun Control', 'Unemployment',
+                                                      'Climate Change', 'Homelessness', 'Racism',
+                                                      'Tax Reform', 'Net Neutrality', 'Religious Freedom',
+                                                      'Border Security', 'Minimum Wage', 'Equal Pay'] }
 
   def self.find_for(representative_id)
     NewsItem.find_by(
